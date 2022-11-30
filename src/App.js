d9b8b311a2b57b2dec48f0c3b7f16ea0de36e629
Marellakarthikreddy <karthikreddymarella66@gmail.com>
2022-11-30 16:52:29 +0530

    30nov2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+    <PropsExample name="mounika" age="20"/>
+    <PropsExample />
+      {/*<p>hi good afternoon all</p>
+      <h1>Have a good day</h1>
+      <img src="https://i.pinimg.com/736x/ce/14/20/ce1420adc861d3be19f003086d5d9930.jpg" alt="nyt view"/>
+      <ClassComponent/>*/}  
+    </section>
+    )
 }
-
 export default App;
